3a87fd366451b0d04046cf3f06051bd44f2c907c
Tyler Mizuyabu <tyler@monanetworks.com>
2017-07-20 13:40:00 -0400

    controller now takes the next notification from the queue and returns it

--- src/server/NotificationController.js
+++ src/server/NotificationController.js
@@ -1,17 +1,29 @@
-const path = require('path');
-const fetch = require('node-fetch');
-const Controller = require('../classes/Controller');
+const path = require("path");
+const fetch = require("node-fetch");
+const squel = require("squel").useFlavour("postgres");
+const db = require("../../database/client");
+const Controller = require("../classes/Controller");
 
-//Create our controller, passing in the script we want to run
-//in the worker proccesses.
 const controller = new Controller({
-  script: path.resolve(__dirname, './notificationWorker.js')
+  script: path.resolve(__dirname, "./notificationWorker.js")
 });
 
 controller.setup();
 controller.run({
   getData: async () => {
-    //Fetch next notification from notification queue
+    var selectQuery = squel
+      .select()
+      .from("notification")
+      .where("notification.sent is NULL")
+      .where("notification.at <= NOW()")
+      .order("notification.at");
+    try {
+      const notification = (await db.manyOrNone(selectQuery.toString()))[0];
+      return {notification};
+    } catch (err) {
+      console.log("error", err);
+    }
+
     return {};
   }
 });
--- src/server/user/create.js
+++ src/server/user/create.js
@@ -1,6 +1,6 @@
-const squel = require('squel').useFlavour('postgres')
-const utils = require('../utils')
-const db = require('../../../database/client')
+const squel = require("squel").useFlavour("postgres");
+const utils = require("../utils");
+const db = require("../../../database/client");
 
 module.exports = async function createUser(ctx) {
   // TODO: Add schema to validate data
@@ -15,60 +15,60 @@ module.exports = async function createUser(ctx) {
     delivery,
     language,
     timezone,
-    active,
-  } = ctx.request.body
+    active
+  } = ctx.request.body;
 
   const baseQuery = squel
     .insert()
-    .into('account')
-    .set('external_id', external_id)
-    .set('name', name)
-    .returning('*')
+    .into("account")
+    .set("external_id", external_id)
+    .set("name", name)
+    .returning("*");
 
   if (email) {
-    baseQuery.set('email', email)
+    baseQuery.set("email", email);
   }
 
   if (sms) {
-    baseQuery.set('sms', sms)
+    baseQuery.set("sms", sms);
   }
 
   if (voice) {
-    baseQuery.set('voice', voice)
+    baseQuery.set("voice", voice);
   }
 
   if (delivery) {
-    baseQuery.set('delivery', utils.pgArr(delivery))
+    baseQuery.set("delivery", utils.pgArr(delivery));
   }
 
   if (timezone) {
-    baseQuery.set('timezone', timezone)
+    baseQuery.set("timezone", timezone);
   }
 
   if (language) {
-    baseQuery.set('language', language)
+    baseQuery.set("language", language);
   } else {
-    baseQuery.set('language', 'en')
+    baseQuery.set("language", "en");
   }
 
   if (active || active == false) {
-    baseQuery.set('timezone', active)
+    baseQuery.set("timezone", active);
   }
 
-  console.log('Running query', baseQuery.toString())
+  console.log("Running query", baseQuery.toString());
 
-  let user
+  let user;
   try {
-    user = await db.one(baseQuery.toString())
+    user = await db.one(baseQuery.toString());
   } catch (err) {
     // TODO: We are assuming that external_id has the only unique constraint
-    if (err.code === '23505') {
-      ctx.response.status = 400
-      ctx.fail({ id: 'this is is already registered for another user' })
-      return
+    if (err.code === "23505") {
+      ctx.response.status = 400;
+      ctx.fail({ id: "this is is already registered for another user" });
+      return;
     }
   }
 
-  console.log('user registered', user)
-  ctx.success(user)
-}
+  console.log("user registered", user);
+  ctx.success(user);
+};
