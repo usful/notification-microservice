d31fbf0ba8d1e247a794b0b7efa11a893b24c631
Tyler Mizuyabu <tyler@monanetworks.com>
2017-09-14 11:04:30 -0400

    removed changes due to misunderstanding

--- src/api/index.js
+++ src/api/index.js
@@ -33,4 +33,4 @@ class API {
   }
 }
 
-module.exports = { API, library };
+module.exports = API;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-const { API, library } = require('./api');
+const API = require('./api');
 const Notificator = require('./notificator');
 
-class NotificationService {
-  constructor() {
-    const notificator = new Notificator();
-    notificator.init();
+const notificator = new Notificator();
+notificator.init();
 
-    const api = new API();
+const api = new API();
 
-    notificator
-      .run()
-      .then(() => console.log('notificator stopped'))
-      .catch(error => console.error('[Error] notificator error', error));
+notificator
+  .run()
+  .then(() => console.log('notificator stopped'))
+  .catch(error => console.error('[Error] notificator error', error));
 
-    api.start();
-
-    return library;
-  }
-}
-
-module.exports = NotificationService;
+api.start();
